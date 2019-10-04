088545e955a7a199ed30c55f0ff11ef4096e9b17
Ka Vang <Ka.Vang@outlook.com>
2019-10-04 15:03:36 -0500

    edited script.js

---
+++ homework3/assets/js/index.js
@@ -0,0 +1,8 @@
+var generateBtn = document.getElementById("generate");
+// document.getElementById("generate").onclick = function() {
+//     prompt("What's Your Name");
+// };
+
+generateBtn.addEventListener('click', function () {
+    prompt ("what");
+})
---
+++ homework3/assets/js/script.js
@@ -0,0 +1,18 @@
+var generateBtn = document.getElementById("generate");
+var keyList = "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()";
+var userChoice = 8-128;
+
+generateBtn.addEventListener('click', function (prompt1) {
+    prompt1 = prompt ("How many characters would you like your password to contain?");
+    for (i = 0; i < keyList.length; i++) {
+        text += cars[i] + "<br>";
+      }
+
+
+    confirm ("Click OK to confirm including special characters.");
+    confirm ("Click OK to confirm including numeric characters.");
+    confirm ("Click OK to confirm including lowercase characters.");
+    confirm ("Click OK to confirm including uppercase characters.");
+
+})
+
