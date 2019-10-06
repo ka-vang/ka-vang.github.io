8be0b01c664d58684d8e3d22e18f9ab2808f9c6f
Ka Vang <Ka.Vang@outlook.com>
2019-10-05 20:54:04 -0500

    deleted index.js, updated script.js

--- homework3/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-var generateBtn = document.getElementById("generate");
-// document.getElementById("generate").onclick = function() {
-//     prompt("What's Your Name");
-// };
-
-generateBtn.addEventListener('click', function () {
-    prompt ("what");
-})
--- homework3/assets/js/script.js
+++ homework3/assets/js/script.js
@@ -1,18 +1,53 @@
-var generateBtn = document.getElementById("generate");
-var keyList = "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()";
-var userChoice = 8-128;
+var generate = document.getElementById("generate");
+var copy = document.getElementById("copy");
 
-generateBtn.addEventListener('click', function (prompt1) {
-    prompt1 = prompt ("How many characters would you like your password to contain?");
-    for (i = 0; i < keyList.length; i++) {
-        text += cars[i] + "<br>";
-      }
+var numeric = '1234567890';
+var special = '~!@#$%^&*()_+';
+var lower = 'qwertyuiopasdfghjklzxcvbnm';
+var upper = 'QWERTYUIOPLKJHGFDSAZXCVBNM';
+var master = '';
+var tempPassword = '';
 
+generate.addEventListener('click', function () {
+  
+  var passwordLength = parseInt(prompt("How many characters would you like your password to contain?"));
 
-    confirm ("Click OK to confirm including special characters.");
-    confirm ("Click OK to confirm including numeric characters.");
-    confirm ("Click OK to confirm including lowercase characters.");
-    confirm ("Click OK to confirm including uppercase characters.");
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert ('Try again. Password must contain 8-128 characters.')
+    }
+    else {
+    var userSpecial = confirm ("Click OK to confirm including special characters.");
+    var userNumber = confirm ("Click OK to confirm including numeric characters.");
+    var userLower = confirm ("Click OK to confirm including lowercase characters.");
+    var userUpper = confirm ("Click OK to confirm including uppercase characters.");
+  }
+
+  if (userSpecial) {
+    master = master + special;
+  }
+
+  if (userNumber) {
+    master = master + numeric;
+  }
+
+  if (userLower) {
+    master = master + lower;
+  }
+
+  if (userUpper) {
+    master = master + upper;
+  }
+
+  for (var i = 0; i < passwordLength; i ++) {
+    tempPassword = tempPassword + master.charAt (Math.floor(Math.random() * Math.floor(master.length)));
+  }
+
+  document.getElementById("password").value = tempPassword;
 
 })
 
+copy.addEventListener('click', function () {
+  document.getElementById("password").select();
+  document.execCommand("copy");
+  alert('Password has been copied');
+})
