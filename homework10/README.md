59916f1f559b5af8965cade9ea0bfc2b3ec3ae61
Ka Vang <Ka.Vang@outlook.com>
2019-11-20 23:32:58 -0600

    started hw 10

---
+++ homework10/README.md
@@ -0,0 +1,17 @@
+# Unit 09 Node.js and ES6+ Homework: Developer Profile Generator
+
+GitHub is an awesome tool but it can be a bit time consuming if all you're looking for is just quick data such as how many repositories they've made, where they're from, and so forth. Worry no more. This application will allow you to quickly pull up a profile on GitHub to include the following information in a simple layout:
+
+    * Profile image
+    * Username
+    * Links to the following:
+    *   User location via Google Maps
+    *   User GitHub profile
+    *   User blog
+    *   User bio
+    * Number of public repositories
+    * Number of followers
+    * Number of GitHub stars
+    * Number of users following
+
+As a bonus, a PDF will be generated as well so in case you're one who prefers hard copies, this is a perfect solution for you. The data pulled is up-to-date with every users' activitiy so you don't have to worry about not having current information on a user. Give this app a try and you'll see how easy it is to use!
---
+++ homework10/index.js
@@ -0,0 +1,75 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const open = require("open");
+const util = require("util");
+const pdf = require('html-pdf');
+const api = require("./api");
+const createHtml = require("./createHtml");
+const http = require("http");
+// const electron = require("electron-pdf");
+
+const prompt = [
+    {
+      type: "input",
+      name: "title",
+      message: "What is your job title?"
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?"
+    },
+];
+
+const writeFile = util.promisify(fs.writeFile);
+
+function writetoFile(data, fileName) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
+
+function init() {
+    inquirer.prompt(prompt)
+        .then(({ github, title }) => {
+            console.log("searching");
+            api.getUser(github)
+                .then(response =>
+                    api.getStars(github)
+                        .then(stars => {
+                            console.log(github)
+                            console.log(response)
+                            return createHtml({stars, title, response})
+                                .then(response =>
+                                    api.getTitle(response)
+                                        .then(title =>{
+                                            console.log(response)
+                                            return createHtml(title)
+                                }))
+                
+                        }
+        ))
+        .then(async function (html) {
+            await writeFile("index.html", html);
+            var readHtml = fs.readFileSync('index.html', 'utf8');
+            // var paperSizeArray = ["A4", "A5"];
+            var options = {
+                height: "900mm",
+                width: "500mm",
+                format: 'Letter'
+                // landscape: false,
+                // marginsType: 0,
+                // printBackground: false,
+                // printSelectionOnly: false,
+            };
+            pdf.create(readHtml, options).toFile('test.pdf', function(err, res) {
+                if (err) return console.log(err);
+                    console.log(res); 
+                });     
+            })
+            .catch(function(error){
+                console.log(error)
+            })
+        })
+}
+
+init();
